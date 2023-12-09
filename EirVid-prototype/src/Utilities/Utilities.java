0b6e9670e5e1643263968e56287b4e942b97295d
Magdalena <xochitl.burtnik@gmail.com>
2023-12-09 17:01:36 +0000

    Utilities class created
    Created a utilities class that will have all utility methods

---
+++ EirVid-prototype/src/Utilities/Utilities.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilities;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2021267
+ */
+public class Utilities {
+    private final Scanner myKB = new Scanner(System.in); // create myKb from scanner
+    
+    /**
+     * Get String value from user
+     * Keep asking until user type it correctly
+     * @param message - message to display to user
+     * @return user input value as String
+     */
+    public String getUserText(String message){
+        String userInput = ""; //set userInput to empty string initially
+        boolean isValid;
+        do{
+            System.out.println(message);
+            try{
+                userInput = this.myKB.nextLine();
+                isValid = true;
+            }catch(Exception e){
+                isValid = false;
+            }   
+        }while(!isValid);
+        return userInput;
+    }
+}
--- EirVid-prototype/src/eirvid/prototype/EirVidPrototype.java
+++ EirVid-prototype/src/eirvid/prototype/EirVidPrototype.java
@@ -4,6 +4,8 @@
  */
 package eirvid.prototype;
 
+import Utilities.Utilities;
+
 /**
  *
  * @author user
@@ -14,7 +16,7 @@ public class EirVidPrototype {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Utilities utility = new Utilities();
     }
     
 }
