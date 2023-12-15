2d6759141e2314e0dcacdffff2b9b4d6cd8c2b11
Magdalena <xochitl.burtnik@gmail.com>
2023-12-15 21:27:56 +0000

    User Managment created
    Created login and register option

--- EirVid-prototype/src/Users/UserManagment.java
+++ EirVid-prototype/src/Users/UserManagment.java
@@ -4,7 +4,10 @@
  */
 package Users;
 
+import DatabaseManagment.Database;
 import Utilities.Utilities;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,38 +19,157 @@ public class UserManagment implements UserManagmentInterface{
     private final Utilities utility = new Utilities();
     // list of registred users, keep in memory for now, later implement database
     private static final Map<String, String> registeredUsers = new HashMap<>();
+    private User currentUser = new User();
+    private Database db;
     
+    public UserManagment(Database db){
+        this.db = db;
+    }
+    
+    /**
+     * Keep asking to login until logged in or exit option selected
+     * @return 
+     */
     @Override
-    public void performLogin(User user) {
-        // Get user email
-        String username = this.utility.getUserText("Enter your username (email): "); // to do add check for email validity later?
-        String password = this.utility.getUserText("Enter your password: "); // to do add minimum password complexity later?
-        
-        // Check if the user is already registered
-        if (this.login(username, password)) {
-            System.out.println("Login successful! Welcome, " + username);
-        } else {
-            System.out.println("Invalid login. Please check your username and password.");
+    public boolean login() {
+        boolean isLogin = true;
+        boolean isLoggedIn = false;
+        while(isLogin && !isLoggedIn){
+            System.out.println("LOGIN");
+            // Get user email
+            String email = this.utility.getUserText("Enter your email: "); // to do add check for email validity later?
+            // Get user password
+            String password = this.utility.getUserText("Enter your password: "); // to do add minimum password complexity later?
+            // create loging user with credentials that user provided
+            User loginUser = new User(email, password);
+            isLoggedIn = this.checkUserCredentials(loginUser);
+            if(!isLoggedIn){
+                int option = this.utility.getUserInteger("Error on login. Type 1 to try again type 2 or any other number to exit");
+                if(option != 1){
+                    isLogin = false;
+                }
+            }
         }
+        return isLoggedIn;
     }
     
     @Override
-    public boolean register(String username, String password) {
-         if (!registeredUsers.containsKey(username)) {
-            // Register the user
-            registeredUsers.put(username, password);
+    public boolean logout() {
+       // Reset current user
+       this.currentUser.setPassword(null);
+       this.currentUser.setEmail(null);
+       return true;
+    }
+    
+    @Override
+    public boolean register() {
+        boolean isRegistering = true;
+        boolean isRegistered = false;
+        while(isRegistering && !isRegistered){
+            System.out.println("REGISTER");
+            // Get email from user
+            String email = this.utility.getUserText("Enter email: "); 
+            // create new user user with credentials that user provided
+            User newUser = new User();
+            newUser.setEmail(email);
+            // if user with that email already exist return false
+            if (this.checkIfUserExists(newUser)){
+                System.out.println("User with that email already exist.");
+                int option = this.utility.getUserInteger("Type 1 to try with other email, type any other number to exit.");
+                if(option == 1){
+                     continue; // Skip the rest of the loop and go to the next iteration
+                }else{
+                    isRegistering = false; // exit the while
+                    continue; // Skip the rest of the loop and go to the next iteration
+                }
+            }
+            
+            // Get password from user
+            String password = this.utility.getUserText("Enter password: "); // to do add minimum password complexity later?
+            newUser.setPassword(password);
+            // insert new user (register) to database
+            if(!insertUserToDatabase(newUser)){
+                // if problem inserting ask again
+                System.out.println("User could not be registred");
+                int option = this.utility.getUserInteger("Type 1 to try wagain, type any other number to exit.");
+                if(option != 1){
+                     isRegistering = false; // if exit selected exit while loop
+                }
+            }else{
+                isRegistered = true;
+            }
+        }
+        return isRegistered;
+    }
+
+    private boolean checkUserCredentials(User user){
+        String useDatabaseByName = "USE " + this.db.getDbName();
+        boolean isSuccess = false;
+        try {
+            this.db.executeStmt(useDatabaseByName);
+            String query = "SELECT * from user \n" +
+                            "WHERE email = '" + user.getEmail() + "'"; // email will be unique so result set should return one value
+            try (ResultSet rs = this.db.executeStmtQuery(query)) {
+                
+                // checking if ResultSet is empty 
+                if (rs.next() == false) { 
+                    System.out.println("No user with that email found."); 
+                } else{
+                    do{
+                        String password = rs.getString("password");
+                        //isSuccess = (password == null ? user.getPassword() == null : password.equals(user.getPassword()));
+                        if(password == null || user.getPassword() == null || !password.equals(user.getPassword())){
+                            System.out.println("Wrong password");
+                        } else{
+                           isSuccess = true;
+                        }
+                    }while(rs.next());
+                }
+                rs.close();
+            }
+            return isSuccess;
+        }catch (SQLException e){
+            System.out.println("Error executing statment to database: " +e.getMessage());
+            return false;
+        }
+    }
+    
+    private boolean checkIfUserExists(User user){
+        String useDatabaseByName = "USE " + this.db.getDbName();
+        boolean isUserFound = false;
+        try {
+            this.db.executeStmt(useDatabaseByName);
+            String query = "SELECT * from user \n" +
+                            "WHERE email = '" + user.getEmail() + "'"; // email should be unique
+            System.out.println(query);
+            try (ResultSet rs = this.db.executeStmtQuery(query)) {
+                if (rs.next() == false) { 
+                    System.out.println("No user with that email found."); 
+                }else{
+                    isUserFound = true;
+                }
+                rs.close();
+            }
+            return isUserFound;
+        }catch (SQLException e){
+            System.out.println("Error executing statment to database: " +e.getMessage());
+            return false;
+        }
+    }
+    
+    private boolean insertUserToDatabase(User user){
+        String useDatabaseByName = "USE " + this.db.getDbName();
+        try {
+            this.db.executeStmt(useDatabaseByName);
+            String query = String.format("INSERT INTO user (email, password) VALUES (\"%s\", \"%s\");",
+                            user.getEmail(), user.getPassword());
+            
+            this.db.executeStmt(query);
             return true;
+        }catch (SQLException e){
+            System.out.println("Error executing statment to database: " +e.getMessage());
+            return false;
         }
-        return false; // User with the given email already exists
-
-    }
-
-    @Override
-    public boolean login(String username, String password) {
-        return registeredUsers.containsKey(username) && registeredUsers.get(username).equals(password);
-    }
-    
-    public void loginScreen(){
         
     }
 
