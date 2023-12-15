e1c58b6f3f9bc94e8f33f6131684b3f6b4c171fa
Gabriel <2021240@student.cct.ie>
2023-12-15 00:14:55 +0000

    Created rent class and interface

---
+++ EirVid-prototype/src/Rent/Rent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rent;
+
+/**
+ * For renting
+ * @author 2021240
+ */
+public class Rent {
+    
+}
